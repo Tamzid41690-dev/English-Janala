763da53e5a8bd663b983ccbe33c6d56074bc9e59
Tamzid41690-dev <tasnimahmed41690@gmail.com>
2025-09-12 21:43:11 +0600

    version-test-1.0.0

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+
+const createElements = (arr) =>{
+    const htmlElements = arr.map(el=> `<span class="btn">${el}</span>`);
+    return htmlElements.join(" ");
+}
+
+const manageInfinity = (status) =>{
+    if(status==true){
+        document.getElementById('infinity').classList.remove("hidden");
+        document.getElementById('word-container').classList.add("hidden");
+    }
+    else{
+        document.getElementById('infinity').classList.add("hidden");
+        document.getElementById('word-container').classList.remove("hidden");
+    }
+}
+
+
 const loadLessons = () =>{
     fetch("https://openapi.programming-hero.com/api/levels/all")
     .then(res => res.json())
@@ -9,7 +27,8 @@ const removeActiveClass = () =>{
 }
 
 const loadWords = (id) =>{
-    const url =`https://openapi.programming-hero.com/api/level/${id}`
+    manageInfinity(true)
+    const url =`https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
     .then(res => res.json())
     .then(data => {
@@ -19,6 +38,35 @@ const loadWords = (id) =>{
          displayloadwords(data.data);
     })
 }
+
+const loadWordDetails = async(id) =>{
+        const url = `https://openapi.programming-hero.com/api/word/${id}`
+        const res = await fetch(url);
+        const details = await res.json();
+        displayWordDetails(details.data);
+}
+const displayWordDetails = (word) =>{
+    console.log(word)
+    const modalContainer = document.getElementById("details-container");
+    modalContainer.innerHTML = `<h1 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i> :${word.pronunciation})</h1>
+      <div>
+        <h2 class="font-bold">Meaning</h2>
+        <p class="font-bangla">${word.meaning}</p>
+      </div>
+      <div>
+        <h2 class="font-bold">Example :</h2>
+        <p>${word.sentence}</p>
+      </div>
+      <div>
+        <h2 class="font-bangla font-bold" >সমার্থক শব্দ গুলো</h2>
+        <div>${createElements(word.synonyms)}</div>
+      </div>
+`;
+    document.getElementById("my_modal").showModal();
+
+}
+
+
 const displayloadwords = (words) =>{
     const wordContainer = document.getElementById("word-container");
    wordContainer.innerHTML = "";
@@ -28,6 +76,8 @@ const displayloadwords = (words) =>{
       <p class="text-[#79716b] font-bangla font-semibold text-2xl">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
       <h2 class="text-4xl font-bold font-bangla">নেক্সট Lesson এ যান</h2>
     </div>`;
+    manageInfinity(false);
+    return;
     }
 
 
@@ -39,12 +89,13 @@ const displayloadwords = (words) =>{
           <p class="font-semibold">Meaning/Pronounciation</p>
           <div class="font-bangla text-2xl font-medium ">${word.meaning ? word.meaning : "কোন অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "কোন উচ্চারণ পাওয়া যায়নি"}</div>
           <div class="flex justify-between items-center mt-5">
-            <button class="btn bg-[#1a91ff1a] hover:bg-[#1a91ff99]"><i class="fa-solid fa-circle-info"></i></button>
+            <button onclick="loadWordDetails(${word.id})" class="btn bg-[#1a91ff1a] hover:bg-[#1a91ff99]"><i class="fa-solid fa-circle-info"></i></button>
             <button class="btn bg-[#1a91ff1a] hover:bg-[#1a91ff99]"><i class="fa-solid fa-volume-high"></i></button>
           </div>
         </div>
         `;
         wordContainer.appendChild(wordDiv);
+        manageInfinity(false);
         
     });
 }
@@ -62,4 +113,5 @@ const displayLessons = (lessons) => {
         
     });
 }
+
 loadLessons();
