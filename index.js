0215c6b798c731f8d45bf37a3b728da11fae26c6
Tamzid41690-dev <tasnimahmed41690@gmail.com>
2025-09-12 18:45:28 +0600

    beta-1.0.0

--- index.js
+++ index.js
@@ -3,6 +3,51 @@ const loadLessons = () =>{
     .then(res => res.json())
     .then(data => displayLessons(data.data))
 }
+const removeActiveClass = () =>{
+    const lessonBtns = document.querySelectorAll(".btn-active");
+    lessonBtns.forEach(btn => btn.classList.remove("btn-active"));
+}
+
+const loadWords = (id) =>{
+    const url =`https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        removeActiveClass();
+        const lessonBtns = document.getElementById(`lesson-btn-${id}`);
+        lessonBtns.classList.add("btn-active");
+         displayloadwords(data.data);
+    })
+}
+const displayloadwords = (words) =>{
+    const wordContainer = document.getElementById("word-container");
+   wordContainer.innerHTML = "";
+    if(words.length === 0){
+        wordContainer.innerHTML = `<div class="text-center  col-span-full space-y-6 p-10">
+      <img src="./assets/alert-error.png" alt="" class="w-24 h-24 mx-auto">
+      <p class="text-[#79716b] font-bangla font-semibold text-2xl">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+      <h2 class="text-4xl font-bold font-bangla">নেক্সট Lesson এ যান</h2>
+    </div>`;
+    }
+
+
+    words.forEach(word => {
+        const wordDiv = document.createElement("div");
+        wordDiv.innerHTML = `
+            <div class="bg-white rounded-2xl py-10 px-5 shadow-sm text-center space-y-5">
+          <h2 class="font-bold text-2xl font-bangla">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
+          <p class="font-semibold">Meaning/Pronounciation</p>
+          <div class="font-bangla text-2xl font-medium ">${word.meaning ? word.meaning : "কোন অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "কোন উচ্চারণ পাওয়া যায়নি"}</div>
+          <div class="flex justify-between items-center mt-5">
+            <button class="btn bg-[#1a91ff1a] hover:bg-[#1a91ff99]"><i class="fa-solid fa-circle-info"></i></button>
+            <button class="btn bg-[#1a91ff1a] hover:bg-[#1a91ff99]"><i class="fa-solid fa-volume-high"></i></button>
+          </div>
+        </div>
+        `;
+        wordContainer.appendChild(wordDiv);
+        
+    });
+}
 
 const displayLessons = (lessons) => {
     const lessonContainer = document.getElementById("lesson-container");
@@ -11,10 +56,10 @@ const displayLessons = (lessons) => {
         const lessonDiv = document.createElement("div");
        
         lessonDiv.innerHTML = `
-           <button class="btn btn-soft btn-primary"><i class="fa-solid fa-book-open"></i>Learn-${lesson.level_no}</button>
+           <button id="lesson-btn-${lesson.level_no}" onclick="loadWords(${lesson.level_no})" class="btn btn-soft btn-primary"><i class="fa-solid fa-book-open"></i>Learn-${lesson.level_no}</button>
         `;
         lessonContainer.appendChild(lessonDiv);
-        console.log(lesson);
+        
     });
 }
 loadLessons();
